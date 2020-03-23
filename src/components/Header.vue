3481dbb7ba2dbe0462704672a540c6d479f72931
Nick <naaltunian@icloud.com>
2020-03-22 20:11:18 -0400

    Create and style header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    Header
+  <div class="ui secondary pointing menu">
+    <a class="active item" href="#">Vue-Go</a>
+    <div class="right menu">
+      <div class="right-navigation">
+        <a class="item" href="#">Users</a>
+        <a class="item" href="#">About</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +17,7 @@ export default {
 </script>
 
 <style scoped>
-
+.right-navigation {
+  display: flex;
+}
 </style>
